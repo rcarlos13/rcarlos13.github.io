d9778854458532814734c447d0da0ba7e9a06776
Renzo Carlos <rcarlos@ryerson.ca>
2020-11-08 15:34:06 -0500

    rebranding and minor updates

--- js/analytics.js
+++ js/analytics.js
@@ -1,5 +1,1 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'G-D2H141HSNP');
+function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","G-D2H141HSNP");
